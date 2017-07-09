64cf762b54dfacc9388a6724e524305f9276af43
onplanetnowhere <onplanetnowhere@gmail.com>
2017-07-09 16:52:33 -0400

    ucks

--- player.lua
+++ player.lua
@@ -2,17 +2,13 @@ Player = Object:extend()
 
 function Player:new()
 	self.image = love.graphics.newImage("player.png")
-<<<<<<< HEAD
 	self.x = 200
 	self.y = 200
+	self.pos = Vec2(300, 200)
 end
 
 function Player:draw()
 	love.graphics.draw(self.image, self.x, self.y)
-end	
-=======
-	self.pos = Vec2(300, 200)
-
 end
 
 function Player:draw()
@@ -20,4 +16,3 @@ function Player:draw()
 	love.graphics.draw(self.image, self.pos.x, self.pos.y)
 
 end
->>>>>>> 0d8e83325b0b21e735186caa81ff5eea4bf61fe1
