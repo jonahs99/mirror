639f926721620e189f2eb8e105d8944035265827
onplanetnowhere <onplanetnowhere@gmail.com>
2017-07-09 16:56:07 -0400

    die

--- player.lua
+++ player.lua
@@ -2,17 +2,9 @@ Player = Object:extend()
 
 function Player:new()
 	self.image = love.graphics.newImage("player.png")
-	self.x = 200
-	self.y = 200
 	self.pos = Vec2(300, 200)
 end
 
 function Player:draw()
-	love.graphics.draw(self.image, self.x, self.y)
-end
-
-function Player:draw()
-
 	love.graphics.draw(self.image, self.pos.x, self.pos.y)
-
 end
