467af419b9161636ae274c921f61e914acfd4914
Jonah Simpson <jonahs99@gmail.com>
2017-07-10 14:13:30 -0400

    fixed vec2 member functions

--- bullet.lua
+++ bullet.lua
@@ -4,10 +4,11 @@ function Bullet:new()
 	self.image = love.graphics.newImage("ping.png")
 	self.pos = Vec2(300,300)
 	self.vel = Vec2(2,0)
+
 end
 
 function Bullet:draw()
 
 	love.graphics.draw(self.image, self.pos.x, self.pos.y)
 
-end	
+end
