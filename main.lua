2852cf67754da9958ec83d7e9f2949fcd2b1b906
Jonah Simpson <jonahs99@gmail.com>
2017-07-09 21:16:23 -0400

    added some random bullets

--- main.lua
+++ main.lua
@@ -5,10 +5,19 @@ function love.load()
 
     require "vec2"
     require "player"
+    require "bullet"
 
     player = Player()
 
-    love.graphics.setBackgroundColor(20,20,20)
+    bullets = {}
+
+    for i=1,2 do
+    	bullets[i] = Bullet()
+    	bullets[i].x = math.random(0, 400)
+    	bullets[i].y = math.random(0, 300)
+    end
+
+    love.graphics.setBackgroundColor(12,14,28)
 end
 
 function love.update(dt)
@@ -17,4 +26,9 @@ end
 
 function love.draw()
 	player:draw()
+
+	for i=1,#bullets do
+		bullets[i]:draw()
+	end
+
 end
