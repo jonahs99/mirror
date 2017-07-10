d49df0836530137e9d927757fe08c0f13ee5f813
Ignas Panero <ignaspan@Ignass-MacBook-Pro.local>
2017-07-10 15:54:37 -0400

    line added

--- main.lua
+++ main.lua
@@ -26,7 +26,9 @@ end
 
 function love.draw()
 	player:draw()
-
+    if clicking then
+        love.graphics.line(startx,starty,endx,endy)
+    end
 	for i=1,#bullets do
 		bullets[i]:draw()
 	end
@@ -35,15 +37,20 @@ end
 
 function shoot()
     if love.mouse.isDown(1) then
-        if !clicking then
+        if not clicking then
             -- Just clicked the mouse button
-            
+            startx = love.mouse.getX()
+            starty = love.mouse.getY()
         end
         clicking = true
+        endx = love.mouse.getX()
+        endy = love.mouse.getY()
     else
         if clicking then
             -- Just lifted the mouse button
-
+            endx = love.mouse.getX()
+            endy = love.mouse.getY()
+            bullets[#bullets+1] = Bullet(player)
         end
         clicking = false
     end
