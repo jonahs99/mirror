3ae975a5afd3e5bda6c6f9ab07fa8ee37c5a66a6
Jonah Simpson <jonahs99@gmail.com>
2017-07-10 15:01:24 -0400

    new images and pixel art style scaling

--- bullet.lua
+++ bullet.lua
@@ -1,16 +1,19 @@
 Bullet = Object:extend()
 function Bullet:new()
-
-	self.image = love.graphics.newImage("ping.png")
+	self.image = love.graphics.newImage("laser.png")
 	self.pos = Vec2(300,300)
 	self.vel = Vec2(2,0)
 
+	-- This makes the scaling pixel-arty
+	self.image:setFilter('nearest', 'nearest')
 end
 
 function Bullet:draw()
-
-	love.graphics.draw(self.image, self.pos.x, self.pos.y, 0, 0.4)
-
+	love.graphics.push()
+	love.graphics.translate(self.pos.x, self.pos.y)
+	love.graphics.scale(4, 4)
+	love.graphics.draw(self.image, 0, 0)
+	love.graphics.pop()
 end	
 
 function Bullet:update()
--- player.lua
+++ player.lua
@@ -3,8 +3,14 @@ Player = Object:extend()
 function Player:new()
 	self.image = love.graphics.newImage("player.png")
 	self.pos = Vec2(300, 200)
+
+	self.image:setFilter('nearest', 'nearest')
 end
 
 function Player:draw()
-	love.graphics.draw(self.image, self.pos.x, self.pos.y)
+	love.graphics.push()
+	love.graphics.translate(self.pos.x, self.pos.y)
+	love.graphics.scale(4, 4)
+	love.graphics.draw(self.image, 0, 0)
+	love.graphics.pop()
 end
